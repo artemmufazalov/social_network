71b9d5dcc62d75673b3a5dedcab60cb14e644bd8
ArtemMufazalov <artemmufazalov6197@gmail.com>
2020-07-23 23:54:06 +0500

    Add API request to (un)follow action. Add credentials to users request

--- src/socialNetwork/components/Users/UsersContainer.js
+++ src/socialNetwork/components/Users/UsersContainer.js
@@ -21,7 +21,9 @@ class UsersContainer extends React.Component {
     getUsers = (pageNumber = 1) => {
         this.props.setIsFetching(true);
         //data.items {id:,name:,status:,photos{small:,large:},followed:}
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${pageNumber}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${pageNumber}`,{
+            withCredentials: true
+        })
             .then(response => {
                 this.props.setUsers(response.data.items);
                 if (this.props.totalUsersCount === 0) {
@@ -36,6 +38,38 @@ class UsersContainer extends React.Component {
         this.getUsers(page);
     }
 
+    onFollow(userId) {
+        axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`, {}, {
+            withCredentials: true,
+            headers: {
+                'API-KEY': '4af23d3e-a1fa-432e-980b-382d82c7d9eb'
+            }
+        })
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    this.props.follow(userId);
+                } else {
+                    console.log(response.data.messages);
+                }
+            });
+    }
+
+    onUnFollow(userId) {
+        axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`, {
+            withCredentials: true,
+            headers: {
+                'API-KEY': '4af23d3e-a1fa-432e-980b-382d82c7d9eb'
+            }
+        })
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    this.props.unfollow(userId);
+                } else {
+                    console.log(response.data.messages);
+                }
+            });
+    }
+
     render() {
         return (
             <>
@@ -45,8 +79,12 @@ class UsersContainer extends React.Component {
                         pageSize={this.props.pageSize}
                         totalUsersCount={this.props.totalUsersCount}
                         currentPage={this.props.currentPage}
-                        follow={this.props.follow}
-                        unfollow={this.props.unfollow}
+                        follow={(userId) => {
+                            this.onFollow(userId)
+                        }}
+                        unfollow={(userId) => {
+                            this.onUnFollow(userId)
+                        }}
                         setCurrentPage={this.props.setCurrentPage}
                         onPageNumberClick={this.onPageNumberClick}
                     />}
