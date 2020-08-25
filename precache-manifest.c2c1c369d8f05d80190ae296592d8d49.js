8793b6677a29763e1b542f37aaa3f3fcfc003e7b
ArtemMufazalov <artemmufazalov6197@gmail.com>
2020-08-25 22:49:32 +0500

    Updates

---
+++ precache-manifest.c2c1c369d8f05d80190ae296592d8d49.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0f00dc6d54b5e2ded10d374718a800",
+    "url": "/social_network/index.html"
+  },
+  {
+    "revision": "876176b5c2c9b358d538",
+    "url": "/social_network/static/css/3.e642f3e8.chunk.css"
+  },
+  {
+    "revision": "b94ab81f711c1ef8a6f1",
+    "url": "/social_network/static/css/main.340f18e6.chunk.css"
+  },
+  {
+    "revision": "5b517cca517ca4a9e779",
+    "url": "/social_network/static/js/2.657acd12.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social_network/static/js/2.657acd12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "876176b5c2c9b358d538",
+    "url": "/social_network/static/js/3.c882aa98.chunk.js"
+  },
+  {
+    "revision": "687c181236cc19c8ea88",
+    "url": "/social_network/static/js/4.11492f18.chunk.js"
+  },
+  {
+    "revision": "b94ab81f711c1ef8a6f1",
+    "url": "/social_network/static/js/main.c5512fd6.chunk.js"
+  },
+  {
+    "revision": "cf35253fec705c6f82dd",
+    "url": "/social_network/static/js/runtime-main.844ee7b0.js"
+  },
+  {
+    "revision": "43003d1185fec4bcf0a98c593552d1b8",
+    "url": "/social_network/static/media/New Americana.43003d11.mp3"
+  },
+  {
+    "revision": "a21d90432938b96db0f22ae69835888c",
+    "url": "/social_network/static/media/No Time To Die.a21d9043.mp3"
+  },
+  {
+    "revision": "b4101e080075e9e0b3c7a70e142b4190",
+    "url": "/social_network/static/media/Spinner-1.3s-200px.b4101e08.svg"
+  },
+  {
+    "revision": "f8b9739423f45f1b3a26d1bf47928b97",
+    "url": "/social_network/static/media/facebook.f8b97394.svg"
+  },
+  {
+    "revision": "9329315b1b1acb1ab416b8dbb86ce46b",
+    "url": "/social_network/static/media/github.9329315b.svg"
+  },
+  {
+    "revision": "759b34fbf239fe538972afde712558e8",
+    "url": "/social_network/static/media/instagram.759b34fb.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/social_network/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b308fa8bd6ee9b1abe416116f0415342",
+    "url": "/social_network/static/media/twitter.b308fa8b.svg"
+  },
+  {
+    "revision": "c57ac9d778d69603ff39604575b3b3f1",
+    "url": "/social_network/static/media/vk.c57ac9d7.svg"
+  },
+  {
+    "revision": "65f3753421853fd869afe206499c91a3",
+    "url": "/social_network/static/media/youtube.65f37534.svg"
+  }
+]);
